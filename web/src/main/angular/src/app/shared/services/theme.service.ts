35eb565f0e8035c11110efa1f9ebb329003095b8
BillionaireDY <doyoonkim1002@gmail.com>
2021-04-28 19:44:28 +0900

    expand theme.service to use dark mode and get current color set

--- web/src/main/angular/src/app/shared/services/theme.service.ts
+++ web/src/main/angular/src/app/shared/services/theme.service.ts
@@ -4,12 +4,15 @@ import * as themeMap from 'app/core/constants/theme';
 
 export const enum Theme {
     Light = 'light',
+    Dark = 'dark',
 }
 
 @Injectable()
 export class ThemeService {
+    currentTheme: Theme = Theme.Light;
     constructor() {}
     private setTheme(theme: Theme): void {
+        this.currentTheme = theme;
         Object.entries(themeMap[theme]).forEach(([key, value]) => {
             document.documentElement.style.setProperty(`--${key}`, value);
         });
@@ -17,5 +20,9 @@ export class ThemeService {
 
     changeTheme(theme: Theme): void {
         this.setTheme(theme);
+    }
+
+    getThemeColor(): any {
+        return this.currentTheme; 
     }
 }

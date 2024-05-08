21d0c0b1a2d37ae75606d919f653a8660688c173
billionaireDY <doyoonkim1002@gmail.com>
2024-05-08 15:24:49 +0900

    [#noissue] feat: split js into vendors and common

--- web-frontend/src/main/v3/apps/web/vite.config.ts
+++ web-frontend/src/main/v3/apps/web/vite.config.ts
@@ -1,8 +1,9 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import { compression } from 'vite-plugin-compression2';
 // import react from '@vitejs/plugin-react';
 import { BASE_PATH } from '@pinpoint-fe/constants';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 const isDev = process.env.NODE_ENV === 'development';
 const target = isDev ? 'http://localhost:8080' : 'http://localhost:8080';
@@ -38,6 +39,11 @@ export default defineConfig({
     },
   },
   plugins: [
+    visualizer({
+      emitFile: false,
+      filename: 'stats.html',
+    }),
+    splitVendorChunkPlugin(),
     compression(),
     compression({
       algorithm: 'brotliCompress',
@@ -45,5 +51,21 @@ export default defineConfig({
       // deleteOriginalAssets: true,
     }),
   ],
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: (id) => {
+  //         // our packages
+  //         if (id.includes('packages/ui')) {
+  //           return '@pinpoint_ui';
+  //         }
+  //         // vendors
+  //         if (id.includes('node_modules')) {
+  //           return '@pinpoint_vendor'; // all other package goes here
+  //         }
+  //       },
+  //     },
+  //   },
+  // },
   base: basePath,
 });
